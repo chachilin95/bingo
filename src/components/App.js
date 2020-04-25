d85496a419b33ff61283ac53fd80aa4b2e8b3ee7
Alejandro Figueroa <arf.me@icloud.com>
2020-04-25 15:33:24 -0500

    Reorganized components

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,20 @@ import React from 'react';
 
 // components
 import BingoSheet from './BingoSheet';
+import BingoSheetGenerator from './BingoSheetGenerator';
+
+import { generateBingoSheet } from '../util/generator';
 
 export default () => {
+
+    const createBingoSheet = ({ numRows, numColumns }) => {
+        console.log('generating');
+        console.log(generateBingoSheet({ numRows, numColumns, maxValue: 75})) // TODO: figure out how to adjust maxValue per number of rows and columns
+    }
+
     return (
         <div>
+            <BingoSheetGenerator handler={(settings) => createBingoSheet(settings)}/>
             <BingoSheet/>
         </div>
     );
--- src/components/BingoSheet/index.js
+++ src/components/BingoSheet/index.js
@@ -1,18 +1,9 @@
-import React, { useState } from 'react';
-
-import { generateBingoSheet } from './util/generator'
-import BingoSheetGenerator from './BingoSheetGenerator';
+import React from 'react';
 
 const BingoSheet = () => {
-
-    const createBingoSheet = ({ numRows, numColumns }) => {
-        console.log(generateBingoSheet({ numRows, numColumns, maxValue: 75})) // TODO
-    }
-
     return (
         <div>
             <h1>Bingo Time!</h1>
-            <BingoSheetGenerator handler={(settings) => createBingoSheet(settings)}/>
         </div>
     );
 };
